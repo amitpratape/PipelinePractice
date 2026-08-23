bfc2f4a695ae1468b3da5ba1ce2bac3eaaaf1298
amitpratape <amitpratape15@gmail.com>
2026-08-24 02:29:56 +0530

    add file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,7 +17,7 @@ test('get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
+test('get started  another link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
